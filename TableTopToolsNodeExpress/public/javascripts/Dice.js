dae063389172f8c96dc4a5e97933f66907116cef
Jttanner <jonttanner@gmail.com>
2018-04-19 18:53:00 -0600

    Add project files.

---
+++ TableTopToolsNodeExpress/public/javascripts/Dice.js
@@ -0,0 +1,32 @@
+﻿function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function calculate_rollDie(sides) {
+    return getRndInteger(1, sides + 1 );
+}
+
+function rollDie(sides) {
+    var rolls = parseInt(document.getElementById("ta_d" + sides).value)
+    if (rolls.length < 1 || isNaN(rolls)) { //TODO: instead disable rolling 
+        rolls = 1;
+    } 
+    var i;
+    var total = 0;
+    for (i = 0; i < rolls; ++i) {
+        //TODO: post each roll to another result window
+        var result = calculate_rollDie(sides);
+        total += result;
+    }
+    var label_answer = "label_d" + sides + "Answer";
+    var answer = document.getElementById(label_answer);
+    answer.textContent = total;
+}
+
+function rollDie_ndx() {
+    var rolls = document.getElementById("ta_ndx_nValue").textContent;
+    var sides = parseInt(document.getElementById("ta_ndx_xValue").value);
+    var result = calculate_rollDie(sides);
+    var answer = document.getElementById("label_ndxAnswer")
+    answer.textContent = result;
+}
